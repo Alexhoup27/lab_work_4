5d272a2b830bd633eca1cb963e83315bd82dc3df
Alexhoup27 <houpalexander@yandex.ru>
2025-03-19 21:22:46 +1000

    1-st version if split 🍑

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 	"strings"
 )
 
+func if_split(data string) []string {
+	count := strings.Count(data, " if ") + strings.Count(data, " if(")
+	result := make([]string, count+1)
+	for i := 0; i < count; i++ {
+		now_ind := min(strings.Index(data, " if "),
+			strings.Index(data, " if("))
+		result[i] = data[:now_ind]
+		data = data[now_ind+4:]
+	}
+	result[count] = data
+	return result
+}
+
 func split(data, delimeter string) []string {
 	result := make([]string, strings.Count(data, delimeter)+1)
 	count := strings.Count(data, delimeter)
