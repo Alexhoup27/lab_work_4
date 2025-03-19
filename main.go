de053d80b8e83766b0858d2ecb9e0de22fb58227
Alexhoup27 <houpalexander@yandex.ru>
2025-03-19 21:06:33 +1000

    new split 😜

--- main.go
+++ main.go
@@ -11,15 +11,10 @@ func split(data, delimeter string) []string {
 	result := make([]string, strings.Count(data, delimeter)+1)
 	count := strings.Count(data, delimeter)
 	for i := 0; i < count; i++ {
-		result[i] = data[:strings.Index(data, delimeter)+len(delimeter)]
+		result[i] = data[:strings.Index(data, delimeter)]
 		data = data[strings.Index(data, delimeter)+1+len(delimeter):]
 	}
-	fmt.Println(count)
 	result[count] = data
-	return result
-}
-func new_split(data, delimeter string) []string {
-	result := make([]string, strings.Count(data, delimeter)+1)
 	return result
 }
 func main() {
@@ -32,5 +27,10 @@ func main() {
 		panic(err_f)
 	}
 	text := string(b_text)
-	fmt.Println(split(text, "package"))
+	to_analyze := split(text, "package")[1:]
+	// fmt.Println(to_analise, len(to_analise))
+	// fmt.Println(strings.Count(text, "package") + 1)
+	for i := 0; i < len(to_analyze); i++ {
+		fmt.Println(to_analyze[i])
+	}
 }
